1eee0fbccb00999bcc88fa3c54fcb5bbd16d3ffc
Vitalii-250885 <provitaleks@gmail.com>
2024-11-05 14:39:39 +0200

    add stiles for site

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+    </main>
+  );
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/AddCompanyButton';
+
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className="text-xl">Home Page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
