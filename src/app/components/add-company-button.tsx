5a1e4b34c599030d6442b3a6c46006324f280db9
Vitalii-250885 <provitaleks@gmail.com>
2024-11-06 16:46:59 +0200

    Update add-company-button.tsx

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,17 +1,14 @@
 'use client';
+
 import React from 'react';
-
-import Button from './button';
 import { useRouter } from 'next/navigation';
+import Button from '@/app/components/button';
 
-function AddCompanyButton() {
+export default function AddCompanyButton() {
   const router = useRouter();
-
   return (
-    <>
-      <Button onClick={() => router.push('/companies/new')}>Add company</Button>
-    </>
+    <Button onClick={() => router.push('/companies/new', { scroll: false })}>
+      Add company
+    </Button>
   );
 }
-
-export default AddCompanyButton;
