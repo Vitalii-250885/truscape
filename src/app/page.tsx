a6792b374accdca767b3f58564446bac87d0e479
Vitalii-250885 <provitaleks@gmail.com>
2024-11-06 16:12:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import React from 'react';
-import { headers } from 'next/headers';
-import AddCompanyButton from './components/add-company-button';
-
 export default function Home() {
-  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Home Page</h1>
-      <AddCompanyButton />
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
